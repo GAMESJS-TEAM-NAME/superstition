ed1bcce6078be1a16f87cf76a75a74572dcc47a9
Faux <nikst0r1963@gmail.com>
2020-03-07 10:09:26 +0200

    uhh

--- superstition/sketch.js
+++ superstition/sketch.js
@@ -5,7 +5,7 @@ function setup() {
     var mgr = new SceneManager();
     mgr.wire();
     //TODO change scene
-    mgr.showScene(PosenetTestScene);
+    mgr.showScene(PoseNetScene);
     window.debugView = true;
 
 }
