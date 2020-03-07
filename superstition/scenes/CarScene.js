1de1b33b6714627255c6196b3599e572b98c9dd3
Faux <nikst0r1963@gmail.com>
2020-03-07 13:12:10 +0200

    Testing car scene

--- superstition/scenes/CarScene.js
+++ superstition/scenes/CarScene.js
@@ -1,29 +1,32 @@
 function CarScene() {
-    // let car;
-    // let bg;
-    // let bloodImg;
-    // let blood;
-    // let player;
+    let car;
+    let bg;
+    let bloodImg;
+    let blood;
+    let player;
     let carSpeed = 0.005;
+    let carAcceleration = 1;
     let carSize = height / 10000;
     let hint = "AVOID THE CAR";
     let loaded = false;
     let loss = false;
+    let win = false;
 
     this.setup = () => {
-        console.log(this.sceneManager);
+        // console.log(this.sceneManager);
 
         // bg = loadImage("./assets/backgrounds/cyberpunk_city.png");
+
         bg = loadImage("./assets/backgrounds/street_desert.png");
         bloodImg = loadImage("./assets/backgrounds/blood.png");
-        cloverImg = loadImage("./assets/clover/loading_clover.png");
-
+        
         car = createSprite(width / 2, height / 1.7, 0, 0);
         car.addAnimation("wheels", "./assets/car/car1.png", "./assets/car/car2.png");
         car.scale = carSize;
-
-        clover = createSprite(width / 2, height / 2, 0, 0);
-        clover.addImage(cloverImg);
+        
+        // cloverImg = loadImage("./assets/clover/loading_clover.png");
+        // clover = createSprite(width / 2, height / 2, 0, 0);
+        // clover.addImage(cloverImg);
 
         hint = createSprite(width/ 2, height / 2, 0, 0);
         let hintAnimation = hint.addAnimation("flicker", "./assets/hints/hint1.png", "./assets/hints/hint2.png");
@@ -36,32 +39,33 @@ function CarScene() {
         blood.visible = false;
 
         player = new PlayerModel(color(255, 0, 0));
-
     }
     
     this.draw = () => {
         if (PoseRecognition.pose || loaded) {
             loaded = true;
-            clover.visible = false;
             hint.visible = false;
             car.visible = true;
-
+            
+            // clover.visible = false;
+            
             background(150);
             image(bg, 0, 0, width, height);
 
-            car.changeAnimation("wheels");
-
-            if (car.scale > width / 2000 && blood.position.y < height / 1.5) {
+            if (loss) {
+                gameOver();
+            } else if (win) {
+                winScreen();
+            } else if (car.scale > width / 2000 && blood.position.y < height / 1.5) {
                 drawSprites();
-                if (PoseRecognition.pose) 
-                    gameOver();
-                else {
-                    console.log("win");
+                if (PoseRecognition.pose) {
+                    loss = true;
                 }
-            } else if (blood.position.y > height / 1.7 && loss) {
-                drawSprites();
-                blood.visible = true;
+                else {
+                    win = true;
+                }
             } else {
+                car.changeAnimation("wheels");
                 drawSprites();
                 updateScale();
                 player.draw();
@@ -69,8 +73,8 @@ function CarScene() {
 
         } else if (!loaded) {
             background(39, 44, 72);
-            clover.visible = false;
-            clover.rotation += 3;
+            // clover.visible = false;
+            // clover.rotation += 3;
             car.visible = false;
 
             hint.changeAnimation("flicker");
@@ -90,20 +94,20 @@ function CarScene() {
         text(str, x, y);
     }
 
-    function win() {
+    function winScreen() {
         background(39, 44, 72);
-        displayText("GJ");
-
+        displayText("GJ", width / 10, width / 2, height / 3.5);
     }
 
     function updateScale() {
-        car.scale += carSpeed * car.scale * 3;
-        car.position.y += 0.4;
+        carAcceleration += 0.1;
+        car.scale += carSpeed * carAcceleration;
     }
 
     function gameOver() {
         blood.visible = true;
-        blood.position.y += 8;
-        loss = true;
+        if (blood.position.y < height / 1.5) 
+            blood.position.y += 8;
+        drawSprites();
     }
 }
