e04d8f540d9df9b307dd83e5391927a766ea7578
Faux <nikst0r1963@gmail.com>
2020-03-07 13:19:56 +0200

    Changed loss screen

--- superstition/scenes/CarScene.js
+++ superstition/scenes/CarScene.js
@@ -1,8 +1,8 @@
 function CarScene() {
+    // let bloodImg;
+    // let blood;
     let car;
     let bg;
-    let bloodImg;
-    let blood;
     let player;
     let carSpeed = 0.005;
     let carAcceleration = 1;
@@ -13,30 +13,31 @@ function CarScene() {
     let win = false;
 
     this.setup = () => {
-        // console.log(this.sceneManager);
-
-        // bg = loadImage("./assets/backgrounds/cyberpunk_city.png");
-
-        bg = loadImage("./assets/backgrounds/street_desert.png");
-        bloodImg = loadImage("./assets/backgrounds/blood.png");
         
-        car = createSprite(width / 2, height / 1.7, 0, 0);
-        car.addAnimation("wheels", "./assets/car/car1.png", "./assets/car/car2.png");
-        car.scale = carSize;
+        // console.log(this.sceneManager);
+        // bloodImg = loadImage("./assets/backgrounds/blood.png");
+        // bg = loadImage("./assets/backgrounds/cyberpunk_city.png");
         
         // cloverImg = loadImage("./assets/clover/loading_clover.png");
         // clover = createSprite(width / 2, height / 2, 0, 0);
         // clover.addImage(cloverImg);
 
-        hint = createSprite(width/ 2, height / 2, 0, 0);
+        // blood = createSprite(width / 2, -height, 0, 0);
+        // blood.addImage(bloodImg);
+        // blood.scale = 1.15;
+        // blood.visible = false;
+
+        bg = loadImage("./assets/backgrounds/street_desert.png");
+        
+        car = createSprite(width / 2, height / 1.7, 0, 0);
+        car.addAnimation("wheels", "./assets/car/car1.png", "./assets/car/car2.png");
+        car.scale = carSize;
+
+        hint = createSprite(width / 2, height / 2, 0, 0);
         let hintAnimation = hint.addAnimation("flicker", "./assets/hints/hint1.png", "./assets/hints/hint2.png");
         hint.scale = height / 2000;
         hintAnimation.frameDelay = 10;
 
-        blood = createSprite(width / 2, -height, 0, 0);
-        blood.addImage(bloodImg);
-        blood.scale = 1.15;
-        blood.visible = false;
 
         player = new PlayerModel(color(255, 0, 0));
     }
@@ -56,8 +57,9 @@ function CarScene() {
                 gameOver();
             } else if (win) {
                 winScreen();
-            } else if (car.scale > width / 2000 && blood.position.y < height / 1.5) {
+            } else if (car.scale > width / 2000) {
                 drawSprites();
+
                 if (PoseRecognition.pose) {
                     loss = true;
                 }
@@ -94,20 +96,23 @@ function CarScene() {
         text(str, x, y);
     }
 
-    function winScreen() {
-        background(39, 44, 72);
-        displayText("GJ", width / 10, width / 2, height / 3.5);
-    }
-
     function updateScale() {
         carAcceleration += 0.1;
         car.scale += carSpeed * carAcceleration;
     }
 
+    function winScreen() {
+        background(39, 44, 72);
+        displayText("GJ", width / 10, width / 2, height / 3.5);
+    }
+
     function gameOver() {
-        blood.visible = true;
-        if (blood.position.y < height / 1.5) 
-            blood.position.y += 8;
-        drawSprites();
+        // blood.visible = true;
+        // if (blood.position.y < height / 1.5) 
+        //     blood.position.y += 8;
+        // drawSprites();
+
+        background(39, 44, 72);
+        displayText("dead", width / 10, width / 2, height / 3.5);
     }
 }
